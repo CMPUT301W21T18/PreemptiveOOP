7eb750f6a8c0c82af77bda80fa771ea057b0a0dc
xuanrui-work <axuanruizeng@gmail.com>
2021-02-21 17:48:38 -0700

    configured Firestore

--- app/src/main/java/com/example/preemptiveoop/MainActivity.java
+++ app/src/main/java/com/example/preemptiveoop/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.preemptiveoop;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // simple test to our database
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        HashMap<String, String> test = new HashMap<>();
+        test.put("description", "A simple test to our Firestore database.");
+
+        db.collection("Test").document("Test #1")
+                .set(test)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) { Log.d("DB", "Test data successfully added."); }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) { Log.d("DB", "Test failed. " + e.toString()); }
+                });
     }
 }
