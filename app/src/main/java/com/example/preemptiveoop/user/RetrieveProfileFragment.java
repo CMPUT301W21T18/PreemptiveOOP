772b057c02dbdf151b6f85acf79509572b0e2c61
xuanrui-work <axuanruizeng@gmail.com>
2021-03-25 16:06:57 -0600

    refactor

--- app/src/main/java/com/example/preemptiveoop/user/RetrieveProfileFragment.java
+++ app/src/main/java/com/example/preemptiveoop/user/RetrieveProfileFragment.java
@@ -15,6 +15,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.preemptiveoop.R;
+import com.example.preemptiveoop.uiwidget.MyDialog;
 import com.example.preemptiveoop.user.model.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,55 +24,54 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RetrieveProfileFragment extends DialogFragment{
-
-    private TextView tvRetrieveResult;
+    private TextView tvResult;
     private EditText etUsername;
-    private Button btRetrieveUserProfile;
-    CollectionReference usrCol = FirebaseFirestore.getInstance().collection("Users");
+    private Button btRetrieve;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_retrieve_profile, null);
 
-        etUsername = view.findViewById(R.id.EditText_search_username);
-        btRetrieveUserProfile = view.findViewById(R.id.Button_retrieve_user);
-        tvRetrieveResult = view.findViewById(R.id.TextView_retrieve_result);
+        etUsername = view.findViewById(R.id.EditText_username);
+        btRetrieve = view.findViewById(R.id.Button_retrieve);
+        tvResult = view.findViewById(R.id.TextView_result);
 
-        btRetrieveUserProfile.setOnClickListener(this::onClick);
+        btRetrieve.setOnClickListener(this::btRetrieveOnClick);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setView(view);
-        builder.setTitle("Retrieve User Profile");
-        builder.setPositiveButton("Confirm", null);
-        builder.setNegativeButton("Cancel", null);
+        builder.setView(view)
+                .setTitle("Retrieve User Profile")
+                .setPositiveButton("OK", null);
         return builder.create();
     }
 
-    public void onClick(View v) {
-        String search_usr_name = etUsername.getText().toString();
-        if (search_usr_name.equals("")) {
-            tvRetrieveResult.setText("Invalid Username");
+    public void btRetrieveOnClick(View v) {
+        String username = etUsername.getText().toString();
+        if (username.isEmpty()) {
+            MyDialog.errorDialog(getActivity(), "Empty Field", "Please enter a username.");
             return;
         }
-        usrCol.document(search_usr_name).get()
+
+        CollectionReference userCol = FirebaseFirestore.getInstance().collection("Users");
+        userCol.document(username).get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if (!documentSnapshot.exists()) {
-                            tvRetrieveResult.setText("Invalid Username");
+                            MyDialog.errorDialog(getActivity(),
+                                    "User Not Found",
+                                    "The provide username was not found in the DB.");
                             return;
                         }
 
                         User user = documentSnapshot.toObject(User.class);
-                        tvRetrieveResult.setText(user.getContact());
-                        Log.d("USER_PROFILE", "User profile retrieve successfully !");
+                        tvResult.setText(user.getContact());
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("USER_PROFILE", "Retrieve Request fail!");
-                    }
+                    public void onFailure(@NonNull Exception e) { Log.d("ReProfileFragment.DB", "Retrieve Request fail!"); }
                 });
     }
 }
