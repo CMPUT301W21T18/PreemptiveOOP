77ac84bdb2116195390acb3873e78a669aa34142
Beikewzh <wzh412000@gmail.com>
2021-04-02 08:45:59 -0600

    barcode
    barcode add
    not finished with scannner

--- app/src/main/java/com/example/preemptiveoop/MainActivity.java
+++ app/src/main/java/com/example/preemptiveoop/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.preemptiveoop.barcode.ScannerActivity;
 import com.example.preemptiveoop.experiment.ExperimentList;
 import com.example.preemptiveoop.user.UserLogin;
 import com.example.preemptiveoop.user.model.User;
@@ -59,7 +60,31 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("MainActivity.user", user);
         startActivity(intent);
     }
+<<<<<<< Updated upstream
     public void btQrcodeOnClick(View v) {}
+=======
+    public void btSearchOnClick(View v) {
+        Intent intent = new Intent(this, ExperimentList.class);
+        intent.putExtra(".user", user);
+        intent.putExtra(".searchMode", true);
+        startActivity(intent);
+    }
+    public void btUsrProfileOnClick(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("user", user);
+        UserProfileFragment fragment = new UserProfileFragment();
+        fragment.setArguments(bundle);
+        fragment.show(getSupportFragmentManager(),"USER_PROFILE");
+    }
+    public void btRetrieveProfileOnClick(View v) {
+        RetrieveProfileFragment fragment = new RetrieveProfileFragment();
+        fragment.show(getSupportFragmentManager(),"RETRIEVE_USER_PROFILE");
+    }
+    public void btQrcodeOnClick(View v) {
+        Intent intent = new Intent(this, ScannerActivity.class);
+        startActivity(intent);
+    }
+>>>>>>> Stashed changes
     public void btPostOnClick(View v) {}
     public void btLogoutOnClick(View v) {}
 }
